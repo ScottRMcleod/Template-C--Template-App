481926b78c488777a73545f8cd1ed54a529bcc00
Scott Mcleod <smcleod_06@hotmail.com>
2016-03-10 14:02:04 +1100

    C# Standard Test Application Template
    this is a simple C Sharp Reference template for building a Code Testing
    Application with a simple button and a txtfile

---
+++ CodeRunner/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CodeRunner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RunCode(object sender, RoutedEventArgs e)
+        {
+            Output(txtInput.Text);
+        }
+
+        private void Output(string value)
+        {
+            txtOutput.Text += value + Environment.NewLine;
+        }
+
+        private void ClearOutput(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text = "";        }
+    }
+ 
+}
